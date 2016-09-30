ad248c1d96193cea6440a5ee5debc59763291c12
Mitch Stark <mitchstark@dhcp90-89.calvin.edu>
2016-09-30 10:30:18 -0400

    Completed Lab 4

---
+++ Lab_4/app/src/main/java/edu/calvin/mjs73/lab4/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.calvin.mjs73.lab4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                Intent activity_2 = new Intent(this, Main2Activity.class);
+                startActivity(activity_2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu, menu);
+        return true;
+    }
+}
